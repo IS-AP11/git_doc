6b4ba4628df6ca9dd54c1aebe06bb77ec8f22a24
IS-AP11 <iseyob@gmail.com>
2025-11-08 09:39:34 -0800

    this for version 5

--- ba.sh
+++ ba.sh
@@ -4,3 +4,4 @@ echo "this is ba.sh file"
 echo "this is for version 2"
 echo "now we have version 3"
 echo "version 4 and we have commited here for the first time in the second tutorial"s
+echo "this for version 5"
